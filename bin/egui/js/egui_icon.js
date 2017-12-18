86c36f1731f790e4d80dc229726e4b6a679442c6
ensomniac <ryan@ensomniac.com>
2017-12-18 14:49:04 -0500

    Updating to version 15.08

--- bin/egui/js/egui_icon.js
+++ bin/egui/js/egui_icon.js
@@ -6,17 +6,11 @@ function EguiIcon(){
     egui.Box.call(this);
 
     this.primitives["label_icon"] = null;
-    this.label_text = "+";
-    this.font_size_mult = 1.0;
     this.text_color = "rgba(0, 0, 0, 0.8)";
     this.text_alignment = "center";
-
+    this.scale = 1.0;
     this.width = -1;
     this.height = egui.line_height;
-
-    this.set_font_size_mult = function(font_size_mult){
-        this.font_size_mult = font_size_mult;
-    };
 
     this.draw_label = function(){
         // Post draw is fired by the inherited box
@@ -40,19 +34,8 @@ function EguiIcon(){
     this.set_post_rect = function(){
 
         var aspect = this.rect.width/this.rect.height;
-        var font_size_mult = 1.0;
-        var font_size = this.rect.height;
-
-        if (aspect > 1) {
-            // Wide label
-            font_size_mult = 0.4;
-            font_size = (this.rect.height*font_size_mult);
-        }
-        else {
-            var aspect_scale_norm = egui.inverse_lerp(0.01, 1, aspect);
-            font_size_mult = egui.lerp(0.1, 0.5, aspect_scale_norm);
-            font_size = (this.rect.width*font_size_mult);
-        }
+        var font_size_mult = 0.5;
+        var font_size = this.rect.height*font_size_mult;
 
         this.primitives["label_icon"].css({
             "width": this.rect.width-(egui.padding*2),
@@ -60,7 +43,7 @@ function EguiIcon(){
             "left": this.rect.left,
             "top": this.rect.top,
             "line-height": this.rect.height + "px",
-            "font-size": font_size*(this.font_size_mult) + "px",
+            "font-size": font_size*(this.scale) + "px",
             "padding-left": egui.padding,
             "padding-right": egui.padding,
             "padding-right": egui.padding,
@@ -71,7 +54,7 @@ function EguiIcon(){
         // This should be the only place an instance of a native platform
         // visual element is created. DIV/DRID (HTML/UNITY)
 
-        this.primitives["label_icon"] = $("<div>" + this.label_text + "</div>");
+        this.primitives["label_icon"] = $('<i class="fa fa-camera-retro"></i>');
         this.primitives["label_icon"].css({
             "position": "absolute",
             "text-align": this.text_alignment,
