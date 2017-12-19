570f88947d2c6bf518f0c38d3cc7e8540a714c0d
ensomniac <ryan@ensomniac.com>
2017-12-19 07:21:03 -0500

    Updating to version 15.82

--- bin/egui/js/egui_image.js
+++ bin/egui/js/egui_image.js
@@ -13,8 +13,17 @@ function EguiImage(){
 
     this.set_path = function(image_path){
         this.image_path = image_path;
-        this.set_background_image(this.image_path);
+        this.set_background_image(this.image_path, function(){
+            console.log("image loaded");
+        });
     };
+
+    this.on_image_size = function(image_path){
+    };
+
+
+
+
 
 };
 
