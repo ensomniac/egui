080db5daf770f769348e37cb4d33c0bb0690622a
ensomniac <ryan@ensomniac.com>
2017-12-20 11:36:02 -0500

    Updating to version 17.27

--- bin/egui/js/egui_modal.js
+++ bin/egui/js/egui_modal.js
@@ -5,6 +5,17 @@ function EguiModal(){
 
     egui.layout.CenterBox.call(this);
 
+    this.set_aspect(1.5);
+    this.set_padding_outer(egui.padding);
+    this.set_shadow(0, egui.padding*0.5, egui.padding*10);
+
+    this.vertical_layout = new egui.layout.Vertical();
+    this.vertical_layout.set_padding_inner(egui.padding);
+
+    this.append(new egui.Box());
+
+    console.log(egui);
+
     this.show = function(){
         console.log("Show modal");
     };
