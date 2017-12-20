bdb4f4a18aa667cbb6ed839c03dbbb95fc51581d
ensomniac <ryan@ensomniac.com>
2017-12-20 18:31:02 -0500

    Updating to version 18.42

--- bin/egui/js/egui_spacer.js
+++ bin/egui/js/egui_spacer.js
@@ -1,11 +1,26 @@
 'use strict';
 
-function EguiSpacer(){
-    this.description = "Use this object to create space between other objects in a layout"
+function EguiSpacer(size_array){
+    this.description = "Use this object to create space between other objects in a layout";
+
+    this.size_array = size_array;
 
     egui.Packable.call(this);
 
+    this.draw = function(){
+    };
 
+    if (this.size_array) {
+
+        if (this.size_array[0]) {
+            this.set_width(this.size_array[0]);
+        };
+
+        if (this.size_array[1]) {
+            this.set_height(this.size_array[1]);
+        };
+
+    };
 
 };
 
