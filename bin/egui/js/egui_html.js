ac41512a814c4e86746a09e91df36d0aa254211a
ensomniac <ryan@ensomniac.com>
2017-12-22 13:18:03 -0500

    Updating to version 18.99

---
+++ bin/egui/js/egui_html.js
@@ -0,0 +1,22 @@
+'use strict';
+
+function EguiHTML(){
+    this.description = "Very light abstraction for drawing non-egui-native elements";
+
+    egui.Box.call(this);
+
+    this.set_background("blue");
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
